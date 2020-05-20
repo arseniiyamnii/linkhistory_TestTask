16e978474efadd26e3a3a04a20994023a68ae212
Arsenii Yamnii <yamnii@ya.ru>
2020-05-20 17:42:43 +0500

    find and create all functions

---
+++ dbms.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import redis
+class dbms():
+    def __init__():
+        self.r=redis.Redis()
--- server.py
+++ server.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify
-app = Flask(__name__)
-fromVar="nothing"
+import Redis
+import json
+import time
+class DBMS():
+    def __init__(self):
+        self.db=redis.Redis()
+    #need test
+    #function to sending something to REDIS
+    def sendToDB(self, content):
+        timeStamp=time.time() #get current time
+        contentJson=json.dumps(content)
+        db.set('item', contentJson)
+    #need test
+    #function to search and return something from REDIS
+    def searchAndReturnFromDB(self, timeFrom, timeTo):
+        pattern="["+str(timeFrom)+"-"+str(timeTo)+"]:*"
+        return(self.db.keys(pattern))
+db=redis.Redis() #initialise Redis
+app = Flask(__name__) #initialise Flask
+#need test
 @app.route('/')
 def index():
     return("use /visited_domains")
-#function to get something in url, and answer somesting(not json)
+#need test
+#function to get something in url, and answer somesting in json
 @app.route('/visited_domains')
 def visited_domains():
     fromVar=request.args.get('from')
     return(jsonify(fromVar))
-@app.route('/api',methods=['POST'])
-def api():
+#need test
+#function to get something in json
+@app.route('/visited_links',methods=['POST'])
+def visited_links():
     content=request.get_json()
     print(content)
     return('DONE')
