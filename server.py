d8af3b61a990562be5730b3831f68c5ae0912008
Arsenii Yamnii <yamnii@ya.ru>
2020-05-20 15:22:17 +0500

    we can get json with POST

--- server.py
+++ server.py
@@ -4,11 +4,16 @@ app = Flask(__name__)
 fromVar="nothing"
 @app.route('/')
 def index():
-    return(fromVar)
+    return("use /visited_domains")
 #function to get something in url, and answer somesting(not json)
-@app.route('/api')
-def api():
+@app.route('/visited_domains')
+def visited_domains():
     fromVar=request.args.get('from')
     return(jsonify(fromVar))
+@app.route('/api',methods=['POST'])
+def api():
+    content=request.get_json()
+    print(content)
+    return('DONE')
 if __name__ == '__main__':
     app.run(debug=True)
