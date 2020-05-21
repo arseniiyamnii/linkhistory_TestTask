09897a8331d49ec8606732daa92c705db6022938
Arsenii Yamnii <yamnii@ya.ru>
2020-05-21 14:53:59 +0500

    ready with DB tests

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 from server import *
+import time
 class TestDBMS(unittest.TestCase):
     def setUp(self):
         self.db=DBMS()
@@ -12,6 +13,12 @@ class TestDBMS(unittest.TestCase):
         self.assertEqual(self.db.findNeededKeys([1,9,3,4,5,6,7,8,9],3,7),[3,4,5,6,7])
     def test_domainParser(self):
         self.assertEqual(self.db.domainParser(["http://google.com","ya.ru/someurl","https://funbox.ru?somevalue=qwerty","https://ya.ru/someurl"]),["google.com","ya.ru","funbox.ru"])
-
+    def test_sendAndSearchinDB(self):
+        timeStampFrom=int(time.time())
+        self.db.sendToDB(["http://google.com","http://yandex.ru"])
+        timeStampTo=int(time.time())
+        self.assertEqual(isinstance(self.db.searchAndReturnFromDB(timeStampFrom,timeStampTo),dict),True)
+        for i in range(timeStampFrom-2,timeStampTo+2):
+            self.db.db.delete(str(i))
 if __name__ == "__main__":
     unittest.main()
